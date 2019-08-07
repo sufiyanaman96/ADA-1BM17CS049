d3d3910e1144d9e8f306c1735ff946cc20d63c59
sufiyanaman96 <53850389+sufiyanaman96@users.noreply.github.com>
2019-08-07 12:27:18 +0530

    Add files via upload

--- max.java
+++ max.java
@@ -1,5 +1,28 @@
 package adalab;
+import java.util.*;
 
 public class max {
-
+ int n;
+ int arr[];
+ static void max(int[] arr,int n)
+ {
+	 int max= arr[0];
+	 for(int i=0;i<n;i++)
+	 { if(arr[i]>max)
+		 max =arr[i];
+	 }
+	 System.out.println("The max number is:"+max);
+ }
+ public static void main( String args[])
+ 	{ 	Scanner in = new Scanner( System.in);
+ 		System.out.println("Enter the maximum number of elements: ");
+ 		int n=in.nextInt();
+ 		int[] arr= new int[n];
+ 		System.out.println("enter the integers");
+ 		for (int i=0; i<n; i++)
+		{
+			arr[i]=in.nextInt();
+		}
+ 		max ( arr, n);
+ 	}
 }
